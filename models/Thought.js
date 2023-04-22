8ee92e1e679351e7c9c3b64c298fa7bc4e9bdfd4
arishorts <arieljschwartz@gmail.com>
2023-04-22 14:48:09 -0700

    completed thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,34 +1,48 @@
 const Joi = require("joi");
-const { Schema, model } = require("mongoose");
+const { Schema, Types, model } = require("mongoose");
 
-reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    // type: mongoose.ObjectId,
-    // default: new ObjectId(),
+reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      // type: mongoose.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => `this is the date : ${date}`,
+    },
   },
-  reactionBody: {
+  { toJSON: { getters: true } }
+);
+
+thoughtSchema = new Schema({
+  thoughtText: {
     type: String,
     required: true,
+    minlength: 1,
     maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now,
-    get: (date) => `this is the date : ${date}`,
+    default: (date) => `this is the date: ${date}`,
   },
-});
-
-thoughtSchema = new Schema({
-  thoughtText: {},
-  createdAt: {},
-  username: {},
+  username: { type: String, required: true },
   reactions: [reactionSchema],
 });
+
+//Schema Settings
+//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
 function validateThought(thought) {
   const schema = {
